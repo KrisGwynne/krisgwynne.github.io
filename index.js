af0b31c92e666d4c8ded639142c80c37bcd3be40
Kris <kris.gwynne@softwire.com>
2021-02-26 11:50:17 +0000

    feat: add call to weather api

--- index.js
+++ index.js
@@ -13,7 +13,23 @@ const  loadJSON = async () => {
     })
 }
 
-const showModal = (modal) => {
+getWeatherAsync = async (cityName) => {
+    const APIKey = "67bbc71d06335597f02d676c338fdd65"
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=metric`;
+    return new Promise((res, rej) => {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", url, false);
+        xmlHttp.onreadystatechange = () => {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                res(JSON.parse(xmlHttp.responseText));
+            }
+        }
+        xmlHttp.send(null);
+    });
+};
+
+const showModal = async (modal, race) => {
+    
     modal.style.display = "block";
 }
 
@@ -21,18 +37,28 @@ const hideModal = (modal) => {
     modal.style.display = "none";
 }
 
+const fillWeatherData = async (modal, race) => {
+    const weatherData = await getWeatherAsync(race);
+    if (!weatherData) {
+        return;
+    }
+    modal.querySelector('.modal-temp').innerHTML = `${weatherData?.main?.temp}&deg;C`;
+}
+
 const addOnClickUpcomingRace  = (data, race) => {
     let upcomingRaceButton = document.getElementById(`upcoming-race-${race}`);
     let modal = document.querySelector(".modal");
     let modalCloseButton = modal.querySelector(".modal-close-btn")
 
-    upcomingRaceButton.onclick = () => {
+    upcomingRaceButton.onclick = async() => {
         if (!data[race]){
             return;
         }
         modal.querySelector('.modal-track-layout').setAttribute('src', data[race].trackSrc);
         modal.querySelector('.modal-title').innerHTML = `${race} track layout`;
-        showModal(modal);
+
+        await fillWeatherData(modal, race);
+        showModal(modal, race);
     }
 
     modalCloseButton.onclick = () => {
@@ -51,6 +77,7 @@ const init = async () => {
 
     addOnClickUpcomingRace(upcomingRaceData, 'bahrain');
     addOnClickUpcomingRace(upcomingRaceData, 'italy');
+
 }
 
 init();
