79b6961f0207d2bd5def7dc1fd4a139e4152b8f1
Kris <kris.gwynne@softwire.com>
2021-03-05 11:36:02 +0000

    fix: use https on api call

--- src/getWeatherApi.js
+++ src/getWeatherApi.js
@@ -1,6 +1,6 @@
 const getWeatherAsync = async (cityName) => {
     const APIKey = "67bbc71d06335597f02d676c338fdd65"
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}&units=metric`;
     return new Promise((res, rej) => {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open("GET", url, false);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import "../sass/main.scss";
 import Modal from "./modal";
 
 
